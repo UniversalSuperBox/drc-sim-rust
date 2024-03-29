6461e9f8ae38aad91960621049a91a2dc0c0410e
Dalton Durst <github@daltondurst.com>
2024-03-28 21:09:50 -0500

    Split it apart a bit, make it the noisiest program known to man

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@
 // SOFTWARE.
 
 
-use std::{net::UdpSocket};
+use core::str;
+use std::{fs::File, io::{BufReader, BufWriter, Write}, net::UdpSocket, os::unix::process};
 use bitter::{BigEndianReader, BitReader};
 use log::{debug, error, info, max_level, trace, LevelFilter};
 use simple_logger;
@@ -48,22 +49,150 @@ const PORT_WII_VID: u16 = 50120;
 //     // payload_size: 11 bit unsigned,
 //     header_2: [u8; 2],
 //     timestamp: u32,
-//     extended_header: u8,
+//     extended_header: u64,
 //     payload: [u8],
 // });
+
+#[derive(Debug)]
+struct WiiUPacket {
+    magic: u8,
+    packet_type: u8,
+    seq_id: u16,
+    init: bool,
+    frame_begin: bool,
+    chunk_end: bool,
+    frame_end: bool,
+    has_timestamp: bool,
+    payload_size: u16,
+    timestamp: u32,
+    extended_header: [u8; 8],
+    payload: Vec<u8>
+}
+
+fn get_vid_socket(ip: &str) -> Result<UdpSocket, std::io::Error> {
+    let bindaddr = format!("{}:{}", ip, PORT_WII_VID);
+
+    return UdpSocket::bind(bindaddr);
+}
+
+fn process_video_packet(packet: &[u8]) -> Option<WiiUPacket> {
+    let mut bits = BigEndianReader::new(&packet);
+
+    let len = bits.refill_lookahead();
+    assert!(len >= 32);
+
+    // first 32 bits of the header tell us what type it is and
+    // where it fits in order
+    let magic = bits.peek(4) as u8;
+    assert!(magic == 15, "Unknown magic {magic}");
+    bits.consume(4);
+    let packet_type = bits.peek(2) as u8;
+    assert!(packet_type == 0, "Unknown packet type {packet_type}");
+    bits.consume(2);
+    let seq_id = bits.peek(10) as u16;
+    bits.consume(10);
+
+    let len = bits.refill_lookahead();
+    assert!(len >= 32);
+
+    // next 32 bits regard what this video packet looks like
+    let init = bits.peek(1) != 0;
+    if init {
+        debug!("init!");
+    }
+    bits.consume(1);
+    let frame_begin = bits.peek(1) != 0;
+    if frame_begin {
+        debug!("frame_begin!");
+    }
+    bits.consume(1);
+    let chunk_end = bits.peek(1) != 0;
+    if chunk_end {
+        debug!("chunk_end!");
+    }
+    bits.consume(1);
+    let frame_end = bits.peek(1) != 0;
+    if frame_end {
+        debug!("frame_end!");
+    }
+    bits.consume(1);
+    let has_timestamp = bits.peek(1) != 0;
+    assert!(has_timestamp, "Packet with no timestamp");
+    bits.consume(1);
+    let expected_payload_size_bytes = bits.peek(11) as u16;
+    bits.consume(11);
+
+    let timestamp = bits.read_u32()?;
+
+    // The next 64 bits have <something else> that isn't the
+    // payload
+    let mut extended_header = [0u8; 8];
+    if ! bits.read_bytes(&mut extended_header) {
+        error!("Packet did not have an extended header");
+        return None
+    }
+
+    let mut payload: Vec<u8> = Vec::with_capacity(expected_payload_size_bytes as usize);
+    let remaining_dgram_bits = bits.bytes_remaining();
+    if remaining_dgram_bits >= expected_payload_size_bytes as usize {
+        loop {
+            let still_unread = expected_payload_size_bytes as usize - payload.len();
+
+            if still_unread == 0 {
+                trace!("Finished reading dgram");
+                break;
+            }
+
+            trace!("Have {still_unread} left to read");
+
+            let first_byte = if still_unread > 8 {
+                1
+            } else {
+                8 - still_unread
+            };
+
+            //TODO: If this is too slow, we could speed it up
+            // with bitter's unsafe API. We just need to make
+            // sure we only consume the bytes we need and only
+            // take the appropriate number of bytes from the u64
+            let consumable = bits.refill_lookahead();
+            let piece = bits.peek(consumable);
+            bits.consume(consumable);
+            let split_piece = &piece.to_be_bytes()[first_byte..8];
+            payload.extend(split_piece);
+        }
+    } else {
+        error!("Somehow we don't have {expected_payload_size_bytes} bits remaining, we have {remaining_dgram_bits:?}");
+        return None
+    }
+    if max_level() >= LevelFilter::Trace {
+        let payload_len = payload.len();
+        trace!("Read {payload_len:?} bytes, expected {expected_payload_size_bytes}");
+    }
+
+    return Some(WiiUPacket{
+        magic: magic,
+        packet_type: packet_type,
+        seq_id: seq_id,
+        init: init,
+        frame_begin: frame_begin,
+        chunk_end: chunk_end,
+        frame_end: frame_end,
+        has_timestamp: has_timestamp,
+        payload_size: expected_payload_size_bytes,
+        timestamp: timestamp,
+        extended_header: extended_header,
+        payload: payload
+    })
+}
 
 fn main() -> std::io::Result<()>{
     simple_logger::init_with_env().unwrap();
     {
         //TODO: Bind to the appropriate IP address (It's usually 192.168.1.11 but could be different)
-        let bindaddr = format!("0.0.0.0:{}", PORT_WII_VID);
+        let video_socket = get_vid_socket("0.0.0.0")?;
 
-        let video_socket = UdpSocket::bind(bindaddr)?;
-
-        let mut processed = 0;
-
-        loop {
-            processed += 1;
+        for _n in 1..=1000 {
             // The max size is really 2017 bytes, but bitter prefers
             // reading 64 bit chunks so we'll fill all the way to 2048
             let mut buf = [0u8; 2048];
@@ -73,103 +202,18 @@ fn main() -> std::io::Result<()>{
             // have at least a single-byte payload.
             if amt < 17 {
                 info!("Packet from {src} was too short at only {amt} bytes");
-                continue
-            }
-
-            let mut bits = BigEndianReader::new(&buf);
-
-            let len = bits.refill_lookahead();
-            assert!(len >= 32);
-
-            // first 32 bits of the header tell us what type it is and
-            // where it fits in order
-            let magic = bits.peek(4) as u8;
-            assert!(magic == 15, "Unknown magic {magic}");
-            bits.consume(4);
-            let packet_type = bits.peek(2) as u8;
-            assert!(packet_type == 0, "Unknown packet type {packet_type}");
-            bits.consume(2);
-            let seq_id = bits.peek(10) as u16;
-            bits.consume(10);
-
-            let len = bits.refill_lookahead();
-            assert!(len >= 32);
-
-            // next 32 bits regard what this video packet looks like
-            let init = bits.peek(1) != 0;
-            if init {
-                debug!("init!");
-            }
-            bits.consume(1);
-            let frame_begin = bits.peek(1) != 0;
-            if frame_begin {
-                debug!("frame_begin!");
-            }
-            bits.consume(1);
-            let chunk_end = bits.peek(1) != 0;
-            if chunk_end {
-                debug!("chunk_end!");
-            }
-            bits.consume(1);
-            let frame_end = bits.peek(1) != 0;
-            if frame_end {
-                debug!("frame_end!");
-            }
-            bits.consume(1);
-            let has_timestamp = bits.peek(1) != 0;
-            assert!(has_timestamp, "Packet with no timestamp");
-            bits.consume(1);
-            let expected_payload_size_bytes = bits.peek(11) as usize;
-            bits.consume(11);
-
-            // The next 64 bits have <something else> that isn't the
-            // payload
-            let mut extended_header = [0u8; 8];
-            if ! bits.read_bytes(&mut extended_header) {
-                error!("Packet did not have an extended header");
                 continue;
             }
 
-            let mut payload: Vec<u8> = Vec::with_capacity(expected_payload_size_bytes);
-            let remaining_dgram_bits = bits.bytes_remaining();
-            if remaining_dgram_bits >= expected_payload_size_bytes {
-                loop {
-                    let payload_len = payload.len();
-                    let still_unread = expected_payload_size_bytes - payload_len;
-
-                    if still_unread == 0 {
-                        break;
-                    }
-
-                    let first_byte = if still_unread > 8 {
-                        1
-                    } else {
-                        8 - still_unread
-                    };
-
-                    //TODO: If this is too slow, we could speed it up
-                    // with bitter's unsafe API. We just need to make
-                    // sure we only consume the bytes we need and only
-                    // take the appropriate number of bytes from the u64
-                    let consumable = bits.refill_lookahead();
-                    let piece = bits.peek(consumable);
-                    bits.consume(consumable);
-                    let split_piece = &piece.to_be_bytes()[first_byte..8];
-                    payload.extend(split_piece);
-
+            let packet = match process_video_packet(&buf) {
+                None => {
+                    error!("Didn't get a packet back from process");
+                    continue
                 }
-            } else {
-                error!("Somehow we don't have {expected_payload_size_bytes} bits remaining, we have {remaining_dgram_bits:?}");
-                continue;
-            }
-            if max_level() >= LevelFilter::Trace {
-                let payload_len = payload.len();
-                trace!("Good: Got a payload of {payload_len:?} bytes, expected {expected_payload_size_bytes}");
-            }
-            if processed > 1000 {
-                info!("Finished {processed} packets");
-                break;
-            }
+                Some(val) => val
+            };
+
+            trace!("{packet:?}");
         }
     }
     Ok(())
