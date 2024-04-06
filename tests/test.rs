fb57876102fd60bddfaea421e189ea3a99f5b91c
Dalton Durst <d@ltondur.st>
2024-04-06 16:19:59 -0500

    Add a proptest for magic

--- tests/test.rs
+++ tests/test.rs
@@ -1,19 +1,21 @@
 use drc_sim_rust_lib::incoming_packet_parser::{process_video_packet, WUPVideoPacket};
+use proptest::prelude::*;
 
 /** Converts a WUPVideoPacket into big-endian bytes as parsed by
 process_video_packet.
 
 I have no idea whether this will work on big-endian systems.
 */
-fn data_from_wupvideopacket(input: WUPVideoPacket) -> Vec<u8> {
-    assert!(
-        input.seq_id <= 1023,
-        "seq_id is only a 10-bit number on the wire."
-    );
-    assert!(
-        input.payload_size <= 2047,
-        "payload_size is only an 11-bit number on the wire."
-    );
+fn data_from_wupvideopacket(input: WUPVideoPacket) -> Result<Vec<u8>, &str> {
+    if input.magic > 15 {
+        return Err("magic is only a 4-bit number on the wire.");
+    }
+    if input.seq_id > 1023 {
+        return Err("seq_id is only a 10-bit number on the wire.");
+    }
+    if input.payload_size > 2047 {
+        return Err("payload_size is only an 11-bit number on the wire.");
+    }
 
     let mut data: Vec<u8> = Vec::new();
     let seq_id = input.seq_id.to_be_bytes();
@@ -41,7 +43,7 @@ fn data_from_wupvideopacket(input: WUPVideoPacket) -> Vec<u8> {
 
     data.extend(input.payload);
 
-    return data;
+    return Ok(data);
 }
 
 const ONES: WUPVideoPacket = WUPVideoPacket {
@@ -86,7 +88,7 @@ const CHRISTMAS_TREE_SLICE: [u8; 17] = [
 #[test]
 fn test_data_from_wupvideopacket_ones() {
     assert_eq!(
-        data_from_wupvideopacket(ONES),
+        data_from_wupvideopacket(ONES).unwrap(),
         [0xF0, 1, 8, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
     );
 }
@@ -94,7 +96,7 @@ fn test_data_from_wupvideopacket_ones() {
 #[test]
 fn test_data_from_wupvideopacket_christmastree() {
     assert_eq!(
-        data_from_wupvideopacket(CHRISTMAS_TREE_PKT),
+        data_from_wupvideopacket(CHRISTMAS_TREE_PKT).unwrap(),
         [
             0xF3, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
             0xFF, 0xFF, 0xFF
@@ -110,17 +112,29 @@ fn christmas_tree_video_packet() {
     );
 }
 
-#[test]
-fn test_data_from_wupvideopacket_magic() {
-    for i in 0..15 {
+proptest! {
+    #[test]
+    fn creates_all_magic_values(m in 0..15) {
+        let m = m as u8;
         let mut packet = ONES.clone();
-        packet.magic = i;
-        let first_byte = i << 4;
+        packet.magic = m;
+        let first_byte = m << 4;
+        println!("{}", first_byte);
         assert_eq!(
-            data_from_wupvideopacket(packet),
+            data_from_wupvideopacket(packet).unwrap(),
             [
                 first_byte, 1, 8, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1
             ]
+        )
+    }
+    #[test]
+    fn fails_on_large_magic(m in 16..255) {
+        let m = m as u8;
+        let mut packet = ONES.clone();
+        packet.magic = m;
+        assert_eq!(
+            data_from_wupvideopacket(packet),
+            Err("magic is only a 4-bit number on the wire.")
         )
     }
 }
